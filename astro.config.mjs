238ec2ca6274cd39cd9f216d46570b5be65e9df1
Eric Sexton <foopis23@gmail.com>
2026-08-07 17:15:47 -0400

    feat: add sitemap + robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://foopis23.github.io/emilee-harper-portfolio",
+  integrations: [sitemap({
+    filter: (page) => page !== 'https://foopis23.github.io/components'
+  })]
+});
