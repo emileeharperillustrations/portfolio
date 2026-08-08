0256030a2c8e64e25185db6af03fc9e665bf97f9
Eric Sexton <foopis23@gmail.com>
2026-08-08 15:58:36 -0400

    fix: view transition issues on mobile

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,20 @@
 import { defineConfig } from 'astro/config';
 
 import sitemap from '@astrojs/sitemap';
+import { visualizer } from "rollup-plugin-visualizer";
+
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://foopis23.github.io/",
   base: "/emilee-harper-portfolio",
-  integrations: [sitemap()]
+  vite: {
+    plugins: [visualizer({
+      emitFile: true,
+      filename: "stats.html",
+    })]
+  },
+  integrations: [
+    sitemap()
+  ]
 });
